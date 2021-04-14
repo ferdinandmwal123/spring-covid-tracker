2724d1b3c4ce9dbd18bcbf3d6bfe461e062ab408
ferdinand <ferdinandmwal@gmail.com>
2021-04-14 14:21:53 +0300

    first commit

---
+++ src/test/java/com/mwalagho/ferdinand/covidtracker/CovidTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mwalagho.ferdinand.covidtracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CovidTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
