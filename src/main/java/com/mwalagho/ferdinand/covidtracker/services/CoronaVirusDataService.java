b872daa72273317d0a1c33aabf2db66627d4bfb5
ferdinand <ferdinandmwal@gmail.com>
2021-04-14 14:30:55 +0300

    Spring service

--- src/main/java/com/mwalagho/ferdinand/covidtracker/services/CoronaVirusDataService.java
+++ src/main/java/com/mwalagho/ferdinand/covidtracker/services/CoronaVirusDataService.java
@@ -1,11 +1,14 @@
 package com.mwalagho.ferdinand.covidtracker.services;
 
+import org.springframework.stereotype.Service;
+
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
+@Service
 public class CoronaVirusDataService {
 
     private static String VIRUS_DATA_URL = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
