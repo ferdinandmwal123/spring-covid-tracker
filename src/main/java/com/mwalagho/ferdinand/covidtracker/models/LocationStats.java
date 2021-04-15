c98ce7d643ceb337e187b03ad1aecbcd7fb596fe
ferdinand <ferdinandmwal@gmail.com>
2021-04-15 15:06:44 +0300

    Location stats members

--- src/main/java/com/mwalagho/ferdinand/covidtracker/models/LocationStats.java
+++ src/main/java/com/mwalagho/ferdinand/covidtracker/models/LocationStats.java
@@ -1,4 +1,31 @@
 package com.mwalagho.ferdinand.covidtracker.models;
 
 public class LocationStats {
+
+    private String state,country;
+    private int latestTotalCases;
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public int getLatestTotalCases() {
+        return latestTotalCases;
+    }
+
+    public void setLatestTotalCases(int latestTotalCases) {
+        this.latestTotalCases = latestTotalCases;
+    }
 }
