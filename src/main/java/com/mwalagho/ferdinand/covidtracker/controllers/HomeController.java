78125cf7a8d6fcdfc4776fd0d89c7d108da0bfc1
ferdinand <ferdinandmwal@gmail.com>
2021-04-19 15:52:01 +0300

    mapping controller

--- src/main/java/com/mwalagho/ferdinand/covidtracker/controllers/HomeController.java
+++ src/main/java/com/mwalagho/ferdinand/covidtracker/controllers/HomeController.java
@@ -1,4 +1,14 @@
 package com.mwalagho.ferdinand.covidtracker.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+//renders html ui
+@Controller
 public class HomeController {
+
+    @GetMapping("/")
+    public String home(){
+        return "home";
+    }
 }
