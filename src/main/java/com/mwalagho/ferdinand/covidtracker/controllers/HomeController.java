c007e516a1de9f9e1f38a4f09c01e8b5377743f0
ferdinand <ferdinandmwal@gmail.com>
2021-04-22 13:00:25 +0300

    introduce local variable

--- src/main/java/com/mwalagho/ferdinand/covidtracker/controllers/HomeController.java
+++ src/main/java/com/mwalagho/ferdinand/covidtracker/controllers/HomeController.java
@@ -1,10 +1,13 @@
 package com.mwalagho.ferdinand.covidtracker.controllers;
 
+import com.mwalagho.ferdinand.covidtracker.models.LocationStats;
 import com.mwalagho.ferdinand.covidtracker.services.CoronaVirusDataService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 //renders html ui
 @Controller
@@ -15,7 +18,9 @@ public class HomeController {
 
     @GetMapping("/")
     public String home(Model model) {
-        model.addAttribute("locationStats", coronaVirusDataService.getAllStats());
+        List<LocationStats> allStats = coronaVirusDataService.getAllStats();
+
+        model.addAttribute("locationStats", );
         return "home";
     }
 }
