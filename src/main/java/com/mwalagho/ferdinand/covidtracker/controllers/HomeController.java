4260c08e9802f379f82107b3261c51670ee93463
ferdinand <ferdinandmwal@gmail.com>
2021-04-24 12:52:57 +0300

    added hr

--- src/main/java/com/mwalagho/ferdinand/covidtracker/controllers/HomeController.java
+++ src/main/java/com/mwalagho/ferdinand/covidtracker/controllers/HomeController.java
@@ -20,6 +20,7 @@ public class HomeController {
     public String home(Model model) {
         List<LocationStats> allStats = coronaVirusDataService.getAllStats();
         int totalReportedCases = allStats.stream().mapToInt(stat -> stat.getLatestTotalCases()).sum();
+        int totalNewCases = allStats.stream().mapToInt(stat -> stat.getDiffFromPrevDay()).sum();
         model.addAttribute("locationStats",allStats );
         model.addAttribute("totalReportedCases",totalReportedCases );
 
